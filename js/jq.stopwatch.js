617884aebccb017c803940cb12a21e12db2e52c3
Ian Channing <ian@klever.co.uk>
2015-10-15 13:15:03 +0200

    New feature, add hundredth's of seconds

--- js/jq.stopwatch.js
+++ js/jq.stopwatch.js
@@ -11,16 +11,17 @@
       var timeHour = $('<span>').addClass('hr').text('00');
       var timeMin = $('<span>').addClass('min').text('00');
       var timeSec = $('<span>').addClass('sec').text('00');
+      var timeHSec = $('<span>').addClass('hsec').text('00');
       var startStopBtn = $('<a>').attr('href', '').addClass('start-stop').text('Start');
       var resetBtn = $('<a>').attr('href', '').addClass('reset').text('Reset');
-      stopwatchFace = stopwatchFace.append(timeHour).append(timeMin).append(timeSec);
+      stopwatchFace = stopwatchFace.append(timeHour).append(timeMin).append(timeSec).append(timeHSec);
       instance.html('').append(stopwatchFace).append(startStopBtn).append(resetBtn);
 
       startStopBtn.bind('click', function(e) {
         e.preventDefault();
         var button = $(this);
         if(button.text() === 'Start') {
-          timer = setInterval(runStopwatch, 1000);
+          timer = setInterval(runStopwatch, 10);
           button.text('Stop');
         } else {
           clearInterval(timer);
@@ -36,10 +37,11 @@
           timeHour.text('00');
           timeMin.text('00');
           timeSec.text('00');
+          timeHSec.text('00');
       });
 
       function runStopwatch() {
-        $(this).chkWatch().tick(timeSec, 60).tick(timeMin, 60).tick(timeHour, 24);
+        $(this).chkWatch().tick(timeHSec, 100).tick(timeSec, 60).tick(timeMin, 60).tick(timeHour, 24);
       }
     });
   };
